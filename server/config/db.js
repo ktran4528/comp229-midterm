bf281dc429af50d466c0d858dd95e4c0050d0b62
Kevin Tran <kevin45283@gmail.com>
2022-10-27 19:37:06 -0400

    push to heroku

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+/*
+file name: db.js
+author: Kevin Tran
+student num: 301154481
+web app name: Comp 229 Midterm
+*/
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  URI:"mongodb+srv://kevin:4ljR8eO2JcIKZptl@cluster0.0nx90.mongodb.net/books229?retryWrites=true&w=majority"
+
+};
